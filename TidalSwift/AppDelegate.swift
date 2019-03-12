3565330fde1cb3fe0223152fdbcead82ff316b71
Melvin Gundlach <4gundlac@informatik.uni-hamburg.de>
2019-03-12 04:53:13 +0100

    Initial Commit

---
+++ TidalSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TidalSwift
+//
+//  Created by Melvin Gundlach on 12.03.19.
+//  Copyright © 2019 Melvin Gundlach. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ TidalSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TidalSwift
+//
+//  Created by Melvin Gundlach on 12.03.19.
+//  Copyright © 2019 Melvin Gundlach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
