d3560f70ab54f9c9a1dbc4e7ccf3999332be5385
Melvin Gundlach <4gundlac@informatik.uni-hamburg.de>
2019-03-28 07:34:04 +0100

    Much more tests (Search & Dates). Added offset to search function. Decodable copyright Bugfix.

--- TidalSwiftTests/TidalSwiftTests.swift
+++ TidalSwiftTests/TidalSwiftTests.swift
@@ -116,61 +116,237 @@ class TidalSwiftTests: XCTestCase {
 	}
 	
 	func testSearchArtist() {
-		let searchResultArtist = session.search(for: "Jacob Collier")
-		XCTAssertEqual(searchResultArtist?.artists.totalNumberOfItems, 1)
-		XCTAssertEqual(searchResultArtist?.artists.offset, 0)
-		XCTAssertEqual(searchResultArtist?.artists.items[0].id, 7553669)
-		XCTAssertEqual(searchResultArtist?.artists.items[0].name, "Jacob Collier")
-		XCTAssertEqual(searchResultArtist?.artists.items[0].url,
+		let searchResult = session.search(for: "Jacob Collier")
+		XCTAssertEqual(searchResult?.artists.totalNumberOfItems, 1)
+		XCTAssertEqual(searchResult?.artists.items[0].id, 7553669)
+		XCTAssertEqual(searchResult?.artists.items[0].name, "Jacob Collier")
+		XCTAssertEqual(searchResult?.artists.items[0].url,
 					   URL(string: "http://www.tidal.com/artist/7553669"))
 		// Interestingly the resulting URL is HTTP instead of HTTPS
-		XCTAssertEqual(searchResultArtist?.artists.items[0].picture,
+		XCTAssertEqual(searchResult?.artists.items[0].picture,
 					   "daaa931c-afc0-4c63-819c-c821393b6a45")
-		XCTAssertNotNil(searchResultArtist?.artists.items[0].popularity)
-		XCTAssertNil(searchResultArtist?.artists.items[0].type)
+		XCTAssertNotNil(searchResult?.artists.items[0].popularity)
+		XCTAssertNil(searchResult?.artists.items[0].type)
 	}
 	
 	func testSearchAlbum() {
-		let searchResultAlbum = session.search(for: "Jacob Collier In My Room")
-		XCTAssertEqual(searchResultAlbum?.albums.totalNumberOfItems, 1)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].id, 59978881)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].title, "In My Room")
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].duration, 3531)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].streamReady, true)
-		XCTAssertNotNil(searchResultAlbum?.albums.items[0].streamStartDate)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].allowStreaming, true)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].numberOfTracks, 11)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].numberOfVideos, 0)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].numberOfVolumes, 1)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].releaseDate,
+		let searchResult = session.search(for: "Jacob Collier In My Room")
+		XCTAssertEqual(searchResult?.albums.totalNumberOfItems, 1)
+		XCTAssertEqual(searchResult?.albums.items[0].id, 59978881)
+		XCTAssertEqual(searchResult?.albums.items[0].title, "In My Room")
+		XCTAssertEqual(searchResult?.albums.items[0].duration, 3531)
+		XCTAssertEqual(searchResult?.albums.items[0].streamReady, true)
+		XCTAssertEqual(searchResult?.albums.items[0].streamStartDate,
+					   DateFormatter.iso8601OptionalTime.date(from: "2016-07-01"))
+		XCTAssertEqual(searchResult?.albums.items[0].allowStreaming, true)
+		XCTAssertEqual(searchResult?.albums.items[0].numberOfTracks, 11)
+		XCTAssertEqual(searchResult?.albums.items[0].numberOfVideos, 0)
+		XCTAssertEqual(searchResult?.albums.items[0].numberOfVolumes, 1)
+		XCTAssertEqual(searchResult?.albums.items[0].releaseDate,
 					   DateFormatter.iso8601OptionalTime.date(from: "2016-07-15"))
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].copyright, "2016 Membran")
-		XCTAssertNotNil(searchResultAlbum?.albums.items[0].popularity)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].audioQuality, "LOSSLESS")
+		XCTAssertEqual(searchResult?.albums.items[0].copyright, "2016 Membran")
+		XCTAssertNotNil(searchResult?.albums.items[0].popularity)
+		XCTAssertEqual(searchResult?.albums.items[0].audioQuality, "LOSSLESS")
 		
 		// Album Artist
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].artists?.count, 1)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].artists?[0].id, 7553669)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].artists?[0].name, "Jacob Collier")
-		XCTAssertNil(searchResultAlbum?.albums.items[0].artists?[0].url)
-		XCTAssertNil(searchResultAlbum?.albums.items[0].artists?[0].picture)
-		XCTAssertNil(searchResultAlbum?.albums.items[0].artists?[0].popularity)
-		XCTAssertEqual(searchResultAlbum?.albums.items[0].artists?[0].type, "MAIN")
+		XCTAssertEqual(searchResult?.albums.items[0].artists?.count, 1)
+		XCTAssertEqual(searchResult?.albums.items[0].artists?[0].id, 7553669)
+		XCTAssertEqual(searchResult?.albums.items[0].artists?[0].name, "Jacob Collier")
+		XCTAssertNil(searchResult?.albums.items[0].artists?[0].url)
+		XCTAssertNil(searchResult?.albums.items[0].artists?[0].picture)
+		XCTAssertNil(searchResult?.albums.items[0].artists?[0].popularity)
+		XCTAssertEqual(searchResult?.albums.items[0].artists?[0].type, "MAIN")
+	}
+	
+	func testSearchPlaylist() {
+		let searchResult = session.search(for: "Barack Obama Speeches")
+		XCTAssertEqual(searchResult?.playlists.totalNumberOfItems, 1)
+		XCTAssertEqual(searchResult?.playlists.items.count, 1)
+		XCTAssertEqual(searchResult?.playlists.items[0].uuid,
+					   "96696a2c-b284-4dd3-8e51-5e0dae44ace0")
+		XCTAssertEqual(searchResult?.playlists.items[0].title, "Barack Obama Speeches")
+		XCTAssertEqual(searchResult?.playlists.items[0].numberOfTracks, 20)
+		XCTAssertEqual(searchResult?.playlists.items[0].numberOfVideos, 0)
+		let description = "Grab inspiration from this collection of No. 44's notable speeches. "
+		XCTAssertEqual(searchResult?.playlists.items[0].description, description)
+		XCTAssertEqual(searchResult?.playlists.items[0].duration, 34170)
+		XCTAssertEqual(searchResult?.playlists.items[0].lastUpdated,
+					   DateFormatter.iso8601OptionalTime.date(from:
+						"2019-02-28T21:14:54.402GMT"))
+		XCTAssertEqual(searchResult?.playlists.items[0].created,
+					   DateFormatter.iso8601OptionalTime.date(from:
+						"2018-01-19T17:56:03.125GMT"))
+		XCTAssertEqual(searchResult?.playlists.items[0].type, "EDITORIAL")
+		XCTAssertEqual(searchResult?.playlists.items[0].publicPlaylist, false)
+		XCTAssertEqual(searchResult?.playlists.items[0].url, URL(string:
+			"http://www.tidal.com/playlist/96696a2c-b284-4dd3-8e51-5e0dae44ace0"))
+		XCTAssertEqual(searchResult?.playlists.items[0].image,
+					   "43f8e4db-769c-40f6-b561-99609aef0c13")
+//		print(searchResult?.playlists.items[0].popularity)
+		XCTAssertEqual(searchResult?.playlists.items[0].squareImage,
+					   "50fbe933-0049-4e0e-be82-2de70b19168e")
+	}
+	
+	func testSearchTrack() {
+		let searchResult = session.search(for: "Jacob Collier In My Room")
+		XCTAssertEqual(searchResult?.tracks.totalNumberOfItems, 1)
+		XCTAssertEqual(searchResult?.tracks.items.count, 1)
+		XCTAssertEqual(searchResult?.tracks.items[0].id, 59978883)
+		XCTAssertEqual(searchResult?.tracks.items[0].title, "In My Room")
+		XCTAssertEqual(searchResult?.tracks.items[0].duration, 289)
+		XCTAssertEqual(searchResult?.tracks.items[0].replayGain, -7.04)
+		XCTAssertEqual(searchResult?.tracks.items[0].peak, 0.944366)
+		XCTAssertEqual(searchResult?.tracks.items[0].allowStreaming, true)
+		XCTAssertEqual(searchResult?.tracks.items[0].streamReady, true)
+		XCTAssertEqual(searchResult?.tracks.items[0].streamStartDate,
+					   DateFormatter.iso8601OptionalTime.date(from: "2016-07-01"))
+		XCTAssertEqual(searchResult?.tracks.items[0].trackNumber, 2)
+		XCTAssertEqual(searchResult?.tracks.items[0].volumeNumber, 1)
+//		print(searchResult?.tracks.items[0].popularity)
+		XCTAssertEqual(searchResult?.tracks.items[0].copyright, "2016 Membran")
+		XCTAssertEqual(searchResult?.tracks.items[0].url,
+					   URL(string: "http://www.tidal.com/track/59978883"))
+		XCTAssertEqual(searchResult?.tracks.items[0].isrc, "US23A1500084")
+		XCTAssertEqual(searchResult?.tracks.items[0].editable, true)
+		XCTAssertEqual(searchResult?.tracks.items[0].explicit, false)
+		XCTAssertEqual(searchResult?.tracks.items[0].audioQuality, "LOSSLESS")
+		
+		// Artists
+		XCTAssertEqual(searchResult?.tracks.items[0].artists.count, 1)
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[0].id, 7553669)
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[0].name, "Jacob Collier")
+//		print(searchResult?.videos.items[0].artists[0].type) // For no reason "Index out of range"
+//		XCTAssertEqual(searchResult?.videos.items[0].artists[0].type, "MAIN")
+		
+		// Album
+		XCTAssertEqual(searchResult?.tracks.items[0].album.id, 59978881)
+		XCTAssertEqual(searchResult?.tracks.items[0].album.title, "In My Room")
+	}
+	
+	func testSearchVideo() {
+		let searchResult = session.search(for:
+			"Jacob Collier With The Love In My Heart")
+		XCTAssertEqual(searchResult?.videos.totalNumberOfItems, 1)
+		XCTAssertEqual(searchResult?.videos.items[0].id, 98785108)
+		XCTAssertEqual(searchResult?.videos.items[0].title,
+					   "With The Love In My Heart")
+		XCTAssertEqual(searchResult?.videos.items[0].volumeNumber, 1)
+		XCTAssertEqual(searchResult?.videos.items[0].trackNumber, 1)
+		XCTAssertEqual(searchResult?.videos.items[0].releaseDate,
+					   DateFormatter.iso8601OptionalTime.date(from: "2018-11-16"))
+		XCTAssertEqual(searchResult?.videos.items[0].imageId,
+					   "94cf59fb-2816-4c40-989d-8aff2365baf9")
+		XCTAssertEqual(searchResult?.videos.items[0].duration, 406)
+		XCTAssertEqual(searchResult?.videos.items[0].quality, "MP4_1080P")
+		XCTAssertEqual(searchResult?.videos.items[0].streamReady, true)
+		XCTAssertEqual(searchResult?.videos.items[0].streamStartDate,
+					   DateFormatter.iso8601OptionalTime.date(from:
+						"2018-11-16T18:00:00.000GMT"))
+		XCTAssertEqual(searchResult?.videos.items[0].allowStreaming, true)
+		XCTAssertEqual(searchResult?.videos.items[0].explicit, false)
+//		print(searchResult?.videos.items[0].popularity)
+		XCTAssertEqual(searchResult?.videos.items[0].type, "Music Video")
+		
+		// Artists
+		XCTAssertEqual(searchResult?.tracks.items[0].artists.count, 3)
+		
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[0].id, 7553669)
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[0].name, "Jacob Collier")
+		XCTAssertEqual(searchResult?.videos.items[0].artists[0].type, "MAIN")
+		
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[1].id, 4631340)
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[1].name, "Metropole Orkest")
+		XCTAssertEqual(searchResult?.videos.items[0].artists[1].type, "MAIN")
+		
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[2].id, 4374293)
+		XCTAssertEqual(searchResult?.tracks.items[0].artists[2].name, "Jules Buckley")
+		XCTAssertEqual(searchResult?.videos.items[0].artists[2].type, "MAIN")
+		
 	}
 	
 	func testSearchTopHit() {
-		let searchResultTopHit = session.search(for: "Jacob Collier")
-		XCTAssertEqual(searchResultTopHit?.topHit?.value.id, 7553669)
-		XCTAssertNotNil(searchResultTopHit?.topHit?.value.popularity)
-		XCTAssertNil(searchResultTopHit?.topHit?.value.uuid)
+		// Artist
+		let searchResultArtist = session.search(for: "Jacob Collier")
+		XCTAssertEqual(searchResultArtist?.topHit?.value.id, 7553669)
+		XCTAssertNil(searchResultArtist?.topHit?.value.uuid)
+//		print(searchResult?.topHit?.value.popularity)
+		
+		// Album
+		let searchResultAlbum = session.search(for: "Jacob Collier In My Room")
+		XCTAssertEqual(searchResultAlbum?.topHit?.value.id, 59978881)
+		XCTAssertNil(searchResultAlbum?.topHit?.value.uuid)
+//		print(searchResult?.topHit?.value.popularity)
+		
+		// Playlist
+		let searchResultPlaylist = session.search(for: "Awesome Jazz")
+		XCTAssertNil(searchResultPlaylist?.topHit?.value.id)
+		XCTAssertEqual(searchResultPlaylist?.topHit?.value.uuid, "a784a00e-8f76-4a67-8624-656a1e80f7ed")
+//		print(searchResult?.topHit?.value.popularity)
+		
+		// Track
+		let searchResultTrack = session.search(for: "Britney Toxic")
+		XCTAssertEqual(searchResultTrack?.topHit?.value.id, 42298)
+		XCTAssertNil(searchResultTrack?.topHit?.value.uuid)
+//		print(searchResult?.topHit?.value.popularity)
+	}
+	
+	func testSearchLimitAndOffset() {
+		let searchResult = session.search(for: "Rolf Zuckowski")
+		XCTAssertEqual(searchResult?.tracks.totalNumberOfItems, 300)
+		XCTAssertEqual(searchResult?.tracks.limit, 50)
+		XCTAssertEqual(searchResult?.tracks.items.count, 50)
+		XCTAssertEqual(searchResult?.tracks.offset, 0)
+		
+		// Test Offset
+		let searchResultWithOffset = session.search(for: "Rolf Zuckowski", offset: 1)
+		XCTAssertEqual(searchResultWithOffset?.tracks.limit, 50)
+		XCTAssertEqual(searchResultWithOffset?.tracks.offset, 1)
+		
+		XCTAssertEqual(searchResult?.tracks.totalNumberOfItems, 300)
+		
+		// Test Limit
+		let searchResultWithLimit = session.search(for: "Rolf Zuckowski", limit: 5)
+		XCTAssertEqual(searchResultWithLimit?.tracks.limit, 5)
+		XCTAssertEqual(searchResultWithLimit?.tracks.offset, 0)
+		
+		XCTAssertEqual(searchResult?.tracks.totalNumberOfItems, 300)
+		
+		// Test Big Offset
+		let searchResultWithBigOffset1 = session.search(for: "Rolf Zuckowski", offset: 301)
+		XCTAssertEqual(searchResultWithBigOffset1?.tracks.limit, 50)
+		XCTAssertEqual(searchResultWithBigOffset1?.tracks.offset, 301)
+		XCTAssertEqual(searchResultWithBigOffset1?.tracks.totalNumberOfItems, 300)
+		XCTAssertEqual(searchResultWithBigOffset1?.tracks.items.count, 0)
+		
+		let searchResultWithBigOffset2 = session.search(for: "Rolf Zuckowski", offset: 275)
+		XCTAssertEqual(searchResultWithBigOffset2?.tracks.limit, 50)
+		XCTAssertEqual(searchResultWithBigOffset2?.tracks.offset, 275)
+		XCTAssertEqual(searchResultWithBigOffset2?.tracks.totalNumberOfItems, 300)
+		XCTAssertEqual(searchResultWithBigOffset2?.tracks.items.count, 25)
+		
+		// Test High Limit
+		let searchResultWithHighLimit = session.search(for: "Rolf Zuckowski", limit: 500)
+		XCTAssertEqual(searchResultWithHighLimit?.tracks.limit, 500)
+		XCTAssertEqual(searchResultWithHighLimit?.tracks.offset, 0)
+		XCTAssertEqual(searchResultWithHighLimit?.tracks.items.count, 300)
+		XCTAssertEqual(searchResultWithHighLimit?.tracks.totalNumberOfItems, 300)
 	}
 	
 	func testDateDecoder() {
-		// Tests if the DateDecoder defined at the bottom of Codable correctly decodes a date. Makes sure there is no time zone switching.
-		let rawString = "2016-07-15"
-		let resultString = "2016-07-15 00:00:00 +0000"
+		// Tests if the DateDecoder defined at the bottom of Codable correctly decodes a date.
+		// Makes sure there is no time zone switching.
+		let rawString =    "2016-07-15"
 		let date = DateFormatter.iso8601OptionalTime.date(from: rawString)!
+		let resultString = "2016-07-15 00:00:00 +0000"
 		XCTAssertEqual(resultString, "\(date)")
+		
+		// Test sub-second accuracy
+		let subSecondString = "2019-03-28T06:49:21.123GMT"
+		let subSecondDate =   DateFormatter.iso8601OptionalTime.date(from: subSecondString)!
+		let wrongResult =     "2019-03-28T06:49:21.000GMT"
+		let subSecondResult = DateFormatter.iso8601OptionalTime.string(from: subSecondDate)
+		XCTAssertNotEqual(wrongResult, subSecondResult)
+		XCTAssertEqual(subSecondString, subSecondResult)
 	}
 
 }
